e41357f290e21c71f14c2f3ed483d653837bf1b3
Hendratio0 <hendraaatio@gmail.com>
2025-05-15 10:37:08 +0700

    add step 1 = alokasikan memori untuk node baru

--- Double_linkedlist.cpp
+++ Double_linkedlist.cpp
@@ -20,4 +20,14 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        string nama;
+        cout << "\nEnter the roll number of the student";
+        cin >> nilai;
+
+        Node *newNode = new Node();
+    }
 }
